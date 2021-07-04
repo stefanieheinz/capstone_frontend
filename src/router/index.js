c9057b8fc91a44f357885fbb86d86a4450218a2e
Stefanie <stefanieheinz0@gmail.com>
2021-07-04 12:46:39 -0600

    Created signup and login components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +20,8 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
 ];
 
 const router = new VueRouter({
